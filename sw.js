3941816f35b1ac0f39ae660dff47fdfdbd636fe5
Abhishek Prasun <abhishekprasunrk@gmail.com>
2019-06-09 03:50:11 +0530

    Updated sw.js 'install event'

--- sw.js
+++ sw.js
@@ -1,11 +1,25 @@
-self.addEventListener('install',event =>{
-    console.log('Install event!');
+const cacheName = 'cache-v1';
+const resourcesToPrecache = [
+    '/',
+    'index.html',
+    'expfunction.js'
+
+];
+
+self.addEventListener('install', event => {
+    console.log('Service worker Install event!');
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(resourcesToPrecache);
+            })
+    )
 });
 
-self.addEventListener('activate',event =>{
+self.addEventListener('activate', event => {
     console.log('Activate event!');
 });
 
-self.addEventListener('install',event =>{
+self.addEventListener('fetch', event => {
     console.log('Fetch intercepted for:', event.request.url);
 });
